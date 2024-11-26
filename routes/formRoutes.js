9f15cd6ec250829c89e16e9e45df48156644a18b
Rudy Gabuco Jr <rudyjr.gabuco@gmail.com>
2024-11-25 17:29:32 -0700

    Update formRoutes.js

--- routes/formRoutes.js
+++ routes/formRoutes.js
@@ -7,14 +7,14 @@ const router = express.Router();
 // Route to get all forms and create a form
 router
     .route('/')
-    .get(authController.isAdmin, formController.getAllForms)
+    .get(authController.isAdmin, formController.getAllForms) // admin only
     .post(formController.createForm);
 
 // Route to get, update and delete a form
 router
     .route('/:id')
     .get(formController.getForm)
-    .patch(formController.updateForm)
-    .delete(authController.isAdmin, formController.deleteForm);
+    .patch(authController.isAdmin, formController.updateForm) // admin only
+    .delete(authController.isAdmin, formController.deleteForm); // admin only
 
 module.exports = router;
